c7e8b67973fa474b06c5f8e570ce0faea1d33bfe
andriputra <andriputra.job@gmail.com>
2026-04-10 08:32:11 +0700

    add serverless vercel

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
+const path = require("node:path");
 const serverless = require("serverless-http");
-const { createApp } = require("../dist/app");
+
+const distApp = path.join(__dirname, "..", "dist", "app.js");
+const { createApp } = require(distApp);
 
 const app = createApp();
 module.exports = serverless(app);
